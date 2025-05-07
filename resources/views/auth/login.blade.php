a738ec0edf695dab8c92307bc4d95e9d4a3c235f
Drew Angell <drew@angelleye.com>
2025-05-06 22:44:54 -0500

    Adds GitHub login button, #4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,4 +44,23 @@
             </x-primary-button>
         </div>
     </form>
+
+    <div class="relative mt-6">
+        <div class="absolute inset-0 flex items-center">
+            <div class="w-full border-t border-gray-300"></div>
+        </div>
+        <div class="relative flex justify-center text-sm">
+            <span class="bg-white px-2 text-gray-500">Or continue with</span>
+        </div>
+    </div>
+
+    <div class="mt-6 flex flex-col items-center">
+        <a href="{{ url('/login/github') }}"
+           class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition ease-in-out duration-150">
+            <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M12 .5C5.73.5.5 5.73.5 12c0 5.09 3.29 9.39 7.86 10.93.58.11.79-.25.79-.56 0-.27-.01-1.16-.02-2.11-3.2.69-3.88-1.54-3.88-1.54-.53-1.34-1.3-1.7-1.3-1.7-1.06-.73.08-.72.08-.72 1.17.08 1.78 1.2 1.78 1.2 1.04 1.78 2.74 1.26 3.41.97.11-.76.41-1.26.74-1.55-2.56-.29-5.26-1.28-5.26-5.71 0-1.26.45-2.3 1.2-3.12-.12-.29-.52-1.46.12-3.05 0 0 .98-.31 3.2 1.19a11.1 11.1 0 0 1 2.92-.39c.99 0 1.99.13 2.92.39 2.22-1.5 3.2-1.19 3.2-1.19.64 1.59.24 2.76.12 3.05.75.82 1.2 1.86 1.2 3.12 0 4.44-2.71 5.42-5.29 5.7.43.37.81 1.1.81 2.22 0 1.6-.01 2.89-.01 3.29 0 .31.21.67.8.56C20.72 21.39 24 17.09 24 12c0-6.27-5.23-11.5-12-11.5z"/>
+            </svg>
+            Login with GitHub
+        </a>
+    </div>
 </x-guest-layout>
